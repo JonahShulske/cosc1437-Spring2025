e4017c5240c2d773caa989ccdbcfc80de33e928b
JonahShulske <jonah.shulske@my.tccd.edu>
2025-02-24 14:04:59 -0600

    Will hopefully come back to this soon. Partway done with notes for Lecture3

---
+++ classwork/Lecture3_Notes/Lecture3_Notes/Source.cpp
@@ -0,0 +1,52 @@
+/* Jonah Shulske
+ * Lecture 3 Notes
+ * 2/24/25 Spring 2025
+ */
+
+
+// INTRODUCTION TO OBJECTS IN PROGRAMMING
+/* Definition of Objects:
+ * General: In everyday life, an object is a tangible item.
+ * Programming: An object is an  of a class in object oriented programming (OOP). Encapsulates both state (attributes) and behavior (methods).
+ * 
+ * Components of an Object:
+ * State (Attributes): Data  in the object (variables)
+ * Behavior (Methods):  that an object can perform (functions)
+ */
+
+// TRANSITION FROM PROCEDURAL TO OBJECT ORIENTED PROGRAMMING (OOP)
+/* Procedural Programming: Data functions are  . Data is passed between functions, oftening leading to
+ * Lack of data integrity, Code duplication, and Difficulties in extending/upgrading programs.
+ * 
+ * Object Oriented Programming: Solves many issues by  data and functions within objects.
+ */
+
+// OVERVIEW OF OBJECT ORIENTED PROGRAMMING
+/* Definition: A programming paradigm centered around  .
+ * Key benefits: Modular and reuseable code; and easier maintenance and organization 
+ */
+
+// CHARACTERISTICS OF OOP
+/* Abstraction: Simplifies complex systems by focusing on  an object does, not how it does it.
+ * 
+ * Encapsulation: Bundles data and behavior, ensuring data integrity by restricting access. Promotes  hiding.
+ * 
+ * Inheritance: New classes (derived/child) inherit attributes and methods from existing classes base/parent).
+ * Promotes code  and hierarchial structures
+ * 
+ * Polymorphism: Allows objects of different clases to be treated as instances of  superclass.
+ * Enables flexibility with method  (same method name with different implementations). 
+ */
+
+// OBJECT RELATIONSHIPS IN OOP
+/* Types of Relationships (using UML notation where applicable):
+ * Composition: A "part of" relationship (room and house)
+ * 
+ * Aggregation: A "has a" relationship (course and students)
+ * 
+ * Dependency: A "uses a" relationship (browser and internet connection).
+ * 
+ * Association: Stronger mututal dependency (bees and flowers)
+ * 
+ * Inheritance: An "is a" relationship (square is a type of shape)
+ */
