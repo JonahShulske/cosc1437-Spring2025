8a87fa70b5780d8dbd929b106537104025630df1
JonahShulske <jonah.shulske@my.tccd.edu>
2025-03-24 15:53:51 -0500

    Finished working on BookStore project. Didn't actually do the print stuff in the book.cpp file, but we just skipped it entirely.

--- classwork/BookStore/BookStore_Vector/bookbag.cpp
+++ classwork/BookStore/BookStore_Vector/bookbag.cpp
@@ -45,52 +45,114 @@ void BookBag::RemoveBook(Book book)
 
 void BookBag::ModifyBook(const Book & book)
 {
+    /* The only attributes that can be modified in a book are the price and quantity.
+     * The parameter book should have a default title and description and only the price/quantity should be set to change. Will only look at price and quantity of book parameter.
+    */ 
+
+    Book defaultBook; // Create an instance a default book
+    for (Book& aBook : books)
+        if (aBook == book)   // Check which attributes to update (modify)
+        {
+            if (book.GetPrice() != defaultBook.GetPrice())
+                aBook.SetPrice(book.GetPrice());
+
+            if (book.GetQuantity() != defaultBook.GetQuantity())
+                if (book.GetQuantity() == 0)
+                    RemoveBook(aBook);
+                else
+                    aBook.SetQuantity(book.GetQuantity());
+        }
 }
 
 int BookBag::GetNumBooksInBag() const
 {
-    return 0;
+    // This is the total number of books including duplicates
+    int total = 0;
+    for (const Book& book : books)
+        total += book.GetQuantity();
+
+    return total;
 }
 
 double BookBag::GetCostOfBag() const
 {
-    return 0.0;
+    // Total cost of all the books, including duplicates
+    double total = 0.0;
+    for (const Book& book : books)
+        total += book.GetQuantity() * book.GetPrice();
+
+    return total;
 }
 
 int BookBag::GetUniqueBookCount() const
 {
-    return 0;
+    return books.size();
 }
 
 void BookBag::PrintTotal() const
 {
+    throw std::runtime_error("PrintTotal() not yet implemented");
 }
 
 void BookBag::PrintDescriptions() const
 {
+    throw std::runtime_error("PrintDescriptions() not yet implemented");
 }
 
 BookBag BookBag::operator+(BookBag rhs)
 {
-    return BookBag();
+    BookBag tempBag(this->customerName, this->currentDate);
+    tempBag.books = this->books;
+
+    for (const Book& book : rhs.books)
+        tempBag.AddBook(book);
+
+    return tempBag;
 }
 
 BookBag BookBag::operator+(Book rhs)
 {
-    return BookBag();
+    BookBag tempBag(this->customerName, this->currentDate);
+    tempBag.books = this->books;
+    tempBag.AddBook(rhs);
+
+    return tempBag;
 }
 
 BookBag BookBag::operator-(Book rhs)
 {
-    return BookBag();
+    BookBag tempBag(this->customerName, this->currentDate);
+    tempBag.books = this->books;
+    tempBag.RemoveBook(rhs);
+
+    return tempBag;
 }
 
 Book & BookBag::operator[](int index)
 {
-    // TODO: insert return statement here
+    if (index < 0 || index >= books.size())
+        throw std::runtime_error("Index out of range");
+
+    return books[index];            // Gets book at array of index   
+
+    // books.at(index) is a balance checker and throws exception if out of range
 }
 
 std::ostream & operator<<(std::ostream & outprint, const BookBag & bag)
 {
-    // TODO: insert return statement here
+    if (bag.books.empty())
+    {
+        outprint << "BOOK BAG IS EMPTY";
+        return outprint;
+    }
+
+    outprint << bag.customerName << "'s Book Bag -" << bag.currentDate << std::endl;
+    outprint << "Number of books: " << bag.GetNumBooksInBag() << "\n" << std::endl;
+    
+    for (const Book& book : bag.books)
+        outprint << book << std::endl;
+
+    outprint << "\n" << "Total: $" << bag.GetCostOfBag();
+
+    return outprint;
 }
