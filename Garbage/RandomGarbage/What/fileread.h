ca659412ca1ec1f323580b94efc8a814635fe57e
Jonah Shulske <jonah.shulske@my.tccd.edu>
2025-04-08 09:35:19 -0500

    I honestly don't know what I'm doing lmao

---
+++ Garbage/RandomGarbage/What/fileread.h
@@ -0,0 +1,16 @@
+#ifndef _FILE_READ_H_
+#define _FILE_READ_H_
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+void WriteFile();
+
+void ReadFile();
+
+void DisplayFile(fstream& output, string const& message);
+
+
+
+#endif // _FILE_READ_H_
---
+++ Garbage/RandomGarbage/What/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "fileread.h"
+
+using namespace std;
+
+int main()
+{
+
+}
